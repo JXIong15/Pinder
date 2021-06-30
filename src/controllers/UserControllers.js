e43ce92d5b39cd264aae7767db4cacbf82219f2a
Diego <diegovillarreal123@gmail.com>
2021-06-30 16:29:48 -0500

    added controller for the User.js

---
+++ src/controllers/UserControllers.js
@@ -0,0 +1,24 @@
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+const db = require("../models");
+
+// Definning a method for modelsControllers
+
+module.exports = {
+    findAll: function (req, res) {
+        db.User
+            .find(req.query)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+
+    findById: function (req, res) {
+        db.User
+            .findById(req.params.id)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err));
+    },
+    
+};
+
