f37be0a51ffa486633d5eb3796f33f515fe5fcc6
Sean Bannon <seannbannon@gmail.com>
2021-06-30 10:32:10 -0500

    login page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import login from './containers/login';
+import "./index.css"
+
 
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
         <Switch>
           <Route exact path="/"component={login}></Route>  
         </Switch>
-      Hello world
+      
     </div>
 
     </Router>
