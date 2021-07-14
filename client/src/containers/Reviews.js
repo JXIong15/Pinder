80ef35599b7ceb54db1f55519e622d48a40d4a90
Pikachu <jouxiong15@gmail.com>
2021-07-13 23:25:14 -0500

    reviews display

--- client/src/containers/Reviews.js
+++ client/src/containers/Reviews.js
@@ -1,41 +1,52 @@
-import React from "react";
-
+import React, { Component } from "react";
+import API from "../utils/API";
 
 
 // ADD THIS AFTER CONNECTING TO MONGODB ATLAS
 // function Review(props) {
 // console.log(props)
-// class Review extends Component {
-function Review() {
-    // state = {
-    //     reviews: []
-    // };
-    console.log("reviews")
-    // // console.log(props);
-    // const {id} = props.match.params
-    // console.log(id)
+class Review extends Component {
+// function Review(props) {
+    // const {id} = props.match.params;
+    // let reviewsArr = [];
 
-    let reviewsArr = [];
-    // componentDidMount() {
-        // API.getReviews(props._id)
-        //     .then(res => {
-        //         reviewsArr = res.data;
-        //         console.log(reviewsArr);
-        //     })
-        //     .catch(err => {
-        //         console.log(err);
-        //     })
-    // }
+    state = {
+        reviewsArr: [],
+        id: this.props.match.params.id
+    }
 
-    // render() {
+
+
+    componentDidMount() {
+        console.log( this.state.id)
+        API.getReviews(this.state.id)
+            .then(res => {
+                this.setState({reviewsArr: res.data.reviews});
+                // console.log(this.state.reviewsArr);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+
+    render() {
     return (
         <div>
             <h1>REVIEWS</h1>
             <p>nkjfkjds df cskfjsdkfhsfhsu wueh kajaslfjdsf dsfhdsjf sdhfeijbdhsf</p>
-            <p></p>
+
+            {this.state.reviewsArr.map(review => {
+                return (
+                    <div>
+                        <h3>{review.reviewer}</h3>
+                        <h3>{review.title}</h3>
+                        <p>{review.body}</p>
+                    </div>
+                );
+            })}
         </div>
     );
-    // }
+    }
 }
 
 export default Review;
