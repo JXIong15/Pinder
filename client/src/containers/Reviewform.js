43ae9e9f55c020cc82cbebed90f4d263d297bb54
Sean Bannon <seannbannon@gmail.com>
2021-07-15 22:58:10 -0500

    reviewform

---
+++ client/src/containers/Reviewform.js
@@ -0,0 +1,64 @@
+import React, {useState} from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button"
+import Card from "react-bootstrap/Card"
+import Container from "react-bootstrap/Container"
+
+
+
+
+function Barney(){
+  return(
+
+<Container className = "formContainer">
+  <Card className = "formsCard"> 
+
+  <Form>
+    <br></br>
+  <Form.Group controlId="exampleForm.ControlInput1">
+    <Form.Label>Your email:</Form.Label>
+    <Form.Control type="email" placeholder="name@example.com" />
+  </Form.Group>
+  <Form.Group controlId="exampleForm.ControlSelect1">
+
+    <br></br>
+
+    <Form.Group controlId="exampleForm.ControlInput1">
+    <Form.Label>Reviewee:</Form.Label>
+    <Form.Control type="email"  />
+  </Form.Group>
+  <Form.Group controlId="exampleForm.ControlSelect1"></Form.Group>
+
+    <br></br>
+
+    <Form.Label>Star Rating:</Form.Label>
+    <Form.Control as="select">
+      <option>1 Star</option>
+      <option>2 Stars</option>
+      <option>3 Stars</option>
+      <option>4 Stars</option>
+      <option>5 Stars</option>
+
+    </Form.Control>
+
+    <br></br>  
+
+  </Form.Group>
+  <Form.Group controlId="exampleForm.ControlTextarea1">
+    <Form.Label>Comments:</Form.Label>
+    <Form.Control as="textarea" rows={3} />
+  </Form.Group>
+</Form>
+</Card>
+</Container>
+
+/* <Button className = "submitBtn" block type="submit" disabled={!validateForm()}>
+Login
+</Button> */
+
+    );
+}
+
+
+export default Barney;
+
