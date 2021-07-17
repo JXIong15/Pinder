0bd58a845acfb51ce40ea32546ef3fe2954bb9d8
Jou Xiong <jouxiong15@gmail.com>
2021-07-16 23:29:39 -0500

    got average of ratings to display

--- client/src/containers/Reviews.js
+++ client/src/containers/Reviews.js
@@ -1,8 +1,3 @@
-// import React, {useState} from "react";
-// import Form from "react-bootstrap/Form";
-// import Button from "react-bootstrap/Button"
-// import Card from "react-bootstrap/Card"
-// import Container from "react-bootstrap/Container"
 import React, { Component } from "react";
 import API from "../utils/API";
 import { Link } from 'react-router-dom';
@@ -11,10 +6,11 @@ class Review extends Component {
     state = {
         reviewsArr: [],
         id: this.props.match.params.id,
-        ratingArr: [],
         reviewer: "",
-        profile: []
+        profile: [],
+        avgRating: ""
     }
+
 
     componentDidMount() {
         API.getReviews(this.state.id)
@@ -22,7 +18,8 @@ class Review extends Component {
                 this.setState({ reviewsArr: res.data.reviews });
                 API.getProfile(res.data.profile)
                     .then(res2 => {
-                        this.setState({ profile: res2.data })
+                        this.setState({ profile: res2.data });
+                        this.getRating();
                     })
                     .catch(err => {
                         console.log(err);
@@ -31,85 +28,18 @@ class Review extends Component {
             .catch(err => {
                 console.log(err);
             })
-
-// // ADD THIS AFTER CONNECTING TO MONGODB ATLAS
-// // function Review(props) {
-// // console.log(props)
-// // function Review() {
-    
-// //     return (
-// //         <div>
-// //             <h1>REVIEWS</h1>
-// //             <p>nkjfkjds df cskfjsdkfhsfhsu wueh kajaslfjdsf dsfhdsjf sdhfeijbdhsf</p>
-        
-// //         </div>
-// //     );
-// // }
-
-
-
-
-// function Review(){
-//   return(
-
-// <Container className = "formContainer">
-//   <Card className = "formsCard"> 
-
-//   <Form>
-//     <br></br>
-//   <Form.Group controlId="exampleForm.ControlInput1">
-//     <Form.Label>Your email:</Form.Label>
-//     <Form.Control type="email" placeholder="name@example.com" />
-//   </Form.Group>
-//   <Form.Group controlId="exampleForm.ControlSelect1">
-
-//     <br></br>
-
-//     <Form.Group controlId="exampleForm.ControlInput1">
-//     <Form.Label>Reviewee:</Form.Label>
-//     <Form.Control type="email"  />
-//   </Form.Group>
-//   <Form.Group controlId="exampleForm.ControlSelect1"></Form.Group>
-
-//     <br></br>
-
-//     <Form.Label>Star Rating:</Form.Label>
-//     <Form.Control as="select">
-//       <option>1 Star</option>
-//       <option>2 Stars</option>
-//       <option>3 Stars</option>
-//       <option>4 Stars</option>
-//       <option>5 Stars</option>
-
-//     </Form.Control>
-
-//     <br></br>  
-
-//   </Form.Group>
-//   <Form.Group controlId="exampleForm.ControlTextarea1">
-//     <Form.Label>Comments:</Form.Label>
-//     <Form.Control as="textarea" rows={3} />
-//   </Form.Group>
-// </Form>
-// </Card>
-// </Container>
-
-
-//     );
-// }
-
-
-// export default Review;
-
-
-
-
-// import { BrowserRouter, Route, Switch } from 'react-router-dom';
-
-
-        // API.getProfile()
     }
 
+
+    getRating = () => {
+        let ratings = [];
+        this.state.reviewsArr.map(review => {
+            ratings.push(review.rating);
+        })
+        const sum = ratings.reduce((a, b) => a + b, 0);
+        const avg = (sum / ratings.length) || 0;
+        this.setState({ avgRating: avg });
+    }
 
     getName = (_id) => {
         API.getProfile(_id)
@@ -124,6 +54,7 @@ class Review extends Component {
     };
 
 
+
     render() {
         return (
             <div>
@@ -133,22 +64,24 @@ class Review extends Component {
                     }}>
                         {this.state.profile.first} {this.state.profile.last}
                     </Link>
+                    <span>{this.state.avgRating}</span>
                 </h1>
 
                 {this.state.reviewsArr.map(review => {
-// keeps running. GET HELP ON THIS
-// this.getName(review.reviewer);
-    console.log("after", this.state.reviewer)
+                    // keeps running. GET HELP ON THIS
+                    // this.getName(review.reviewer);
+                    // console.log("after", this.state.reviewer)
+                    // console.log(review)
                     return (
                         <div>
                             <h3>
                                 {/* <Link to={{
                                     pathname: `/profile/${this.state.reviewer._id}`
                                 }}> */}
-                                    {this.state.reviewer}
+                                {this.state.reviewer}
                                 {/* </Link> */}
                             </h3>
-                            <h3>{review.title}</h3>
+                            <h3>{review.title} <span>Rating: {review.rating}</span></h3>
                             <p>{review.body}</p>
                         </div>
                     );
@@ -156,92 +89,6 @@ class Review extends Component {
             </div>
         );
     }
-
-
-// import React, {useRef, useState} from "react";
-// import Form from "react-bootstrap/Form";
-// import Button from "react-bootstrap/Button"
-// import Card from "react-bootstrap/Card"
-// import Container from "react-bootstrap/Container"
-// import { useHistory, Link } from "react-router-dom";
-// import axios from "axios";
-
-
-
-// // ADD THIS AFTER CONNECTING TO MONGODB ATLAS
-// // function Review(props) {
-// // console.log(props)
-// function Review() {
-    
-//     return (
-//         <div>
-//             <h1>REVIEWS</h1>
-//             <p>nkjfkjds df cskfjsdkfhsfhsu wueh kajaslfjdsf dsfhdsjf sdhfeijbdhsf</p>
-        
-//         </div>
-//     );
-
 }
 
 export default Review;
-
-
-
-
-// // import { BrowserRouter, Route, Switch } from 'react-router-dom';
-
-// function Login(props){
-
-//   let email = useRef()
-//   let password = useRef()
-//   let history = useHistory()
-
-//     const handleSubmit = (event) => {
-//         event.preventDefault();
-//         let currentUser = {
-//           email: email.current.value,
-//           password: password.current.value,
-//         }
-//         console.log(currentUser)
-//         axios
-//         .post("/login", currentUser)
-//         .then((data) => {
-//           props.setLogin(data.data)
-//           history.push("/matches")
-
-//         }).catch((err) => {
-//           console.log(err)
-//         })
-//     }
-
-//     return (
-//         <Container className = "container">
-//         <Card className = "loginCard"> 
-//           <Card.Header>Login</Card.Header>
-//       <Form className = "loginForm" onSubmit={handleSubmit}>
-//         <Form.Group className = "loginGroup" size="lg" controlId="email">
-//           <Form.Label className = "formLabel">Email</Form.Label>
-//           <Form.Control
-//             autoFocus
-//             type="email"
-//             ref = {email}
-//           />
-//         </Form.Group>
-//         <Form.Group className = "loginGroup" size="lg" controlId="password">
-//           <Form.Label className = "formLabel">Password</Form.Label>
-//           <Form.Control
-//             type="password"
-//             ref = {password}
-//           />
-//         </Form.Group>
-//         <Button className = "loginBtn" block type="submit">
-//           Login
-//         </Button>
-//       </Form>
-//       </Card>
-//       </Container>
-//         );
-    
-// }
-
-// export default Login;
